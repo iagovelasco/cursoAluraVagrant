cc22471096ade754368fc97b07e92ef9c2f7eb9e
Iago Velasco <iagovelasco@hotmail.com>
2017-04-03 21:41:51 -0300

    vagrant

--- manifests/web.pp
+++ manifests/web.pp
@@ -3,9 +3,31 @@ exec{ "apt-update":
 }
 
 
-package { ["openjdk-7-jre", "tomcat7", "unzip"]:
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
 	ensure => installed,
 	require => Exec["apt-update"]
+}
+
+service{ "mysql":
+		ensure => running,
+		enable => true,
+		hasstatus => true,
+		require => Package["mysql-server"]
+	
+}
+
+exec{ "musicjungle":
+	command => "mysqladmin -uroot create musicjungle",
+	unless => "mysql -u root musicjungle",
+	path => "/usr/bin",
+	require => Service["mysql"]	
+}
+
+exec{"musicjungle":
+	command => "mysql -uroot -e "GRANT ALL PRIVILEGES ON * TO 'musicjungle'@'%' IDENTIFIED BY '1234';" musicjungle",
+	unless => "mysql -umusicjungle -p1234 musicjungle",
+	path => "/usr/bin",
+	require => Service["mysql"]
 }
 
 service{ "tomcat7":
